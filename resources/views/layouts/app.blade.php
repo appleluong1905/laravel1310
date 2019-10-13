2993c112547d91e89214339f39c82ecd8554fe66
Chad Luong <apple.luong1905@gmail.com>
2019-10-13 15:37:42 +0700

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('frontend/css/style.css')}}">
 </head>
 <body>
     <div id="app">
